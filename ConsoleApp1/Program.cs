ca6d38cfe535f1efc7bf7fb3f4c10501caaf5389
Memme_ca81 <Memme_ca81@STHQ0115-11>
2024-02-28 19:20:03 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+MailMessage mailMessage = new MailMessage();
+mailMessage.From = new MailAddress("huseyn200825@gmail.com");
+mailMessage.To.Add("talibliibrahim28@gmail.com");
+mailMessage.Subject = "Salammm";
+mailMessage.Body = "Necesen";
+
+SmtpClient smtpClient = new SmtpClient();
+smtpClient.Host = "smtp.gmail.com";
+smtpClient.Port = 587;
+smtpClient.UseDefaultCredentials = false;
+smtpClient.Credentials = new NetworkCredential("huseyn200825@gmail.com", "imdq ayxq fneu nmbf");
+smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+smtpClient.EnableSsl = true;
+smtpClient.Send(mailMessage);
